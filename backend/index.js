ef686c2411356fbd4ce7f793ba82ed89bb8a859b
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-10-01 18:03:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const todoRoute = require('./routes/todoRoutes');
 const app= express();
 app.use(cors(
     {
-        origin:"*",
+        origin:["https://todoproject-frontend.vercel.app"],
         methods:["POST","GET","DELETE","PUT"],
         credentials:true
     }
